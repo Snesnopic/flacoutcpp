062eeafc7d3961031a84c0614a0a5c49df15f8af
Snesnopic <snesnopic@gmail.com>
2026-06-04 08:40:30 +0200

    Remove old tests, update workflow versions

--- test_overflow.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <limits>
-int main() {
-    uint32_t cost = std::numeric_limits<uint32_t>::max();
-    uint32_t bits = 16 + 4096 * 16;
-    uint32_t sum = cost + bits;
-    std::cout << sum << "\n";
-    return 0;
-}
